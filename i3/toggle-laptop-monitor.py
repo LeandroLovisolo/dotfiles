f4ac7505e42ba7f1e54fb310fc192f96abe7e6f2
Leandro Lovisolo <leandro@leandro.me>
2015-12-17 18:00:30 +0100

    i3: shortcut for toggling laptop display

---
+++ i3/toggle-laptop-monitor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+
+LAPTOP_DISPLAY = 'eDP1'
+EXTERNAL_DISPLAY = 'VGA1'
+TMP_FILE = '~/.i3/.laptop-monitor-active'
+
+import os
+from subprocess import call
+
+TMP_FILE = os.path.expanduser(TMP_FILE)
+
+def turn_on():
+  call(['xrandr', '--output', LAPTOP_DISPLAY, '--auto',
+        '--left-of', EXTERNAL_DISPLAY])
+
+def turn_off():
+  call(['xrandr', '--output', LAPTOP_DISPLAY, '--off'])
+
+if os.path.isfile(TMP_FILE):
+  turn_off()
+  os.remove(TMP_FILE)
+else:
+  turn_on()
+  f = open(TMP_FILE, 'w')
+  f.close()
